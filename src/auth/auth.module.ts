ba434da75385edc7b2018014b5442b023b9f9a88
Mastik14 <www.sergeypygach@mail.ru>
2019-04-29 09:25:26 +0300

    first commit

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,43 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+
+// third-party modules
+import { AngularFireModule, FirebaseAppConfig } from 'angularfire2';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+
+// shared modules
+import { SharedModule } from './shared/shared.module';
+
+export const ROUTES: Routes = [
+  {
+    path: 'auth',
+    children: [
+      { path: '', pathMatch: 'full', redirectTo: 'login' },
+      { path: 'login', loadChildren: './login/login.module#LoginModule' },
+      { path: 'register', loadChildren: './register/register.module#RegisterModule' },
+    ]
+  }
+];
+
+export const firebaseConfig: FirebaseAppConfig = {
+  apiKey: 'AIzaSyD-YgaCCS3Zu_vYGI_bq7-T7S9Ypx6Mag8',
+  authDomain: 'fitness-app-35dc4.firebaseapp.com',
+  databaseURL: 'https://fitness-app-35dc4.firebaseio.com',
+  projectId: 'fitness-app-35dc4',
+  storageBucket: 'fitness-app-35dc4.appspot.com',
+  messagingSenderId: '429125376588'
+};
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forChild(ROUTES),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireAuthModule,
+    AngularFireDatabaseModule,
+    SharedModule.forRoot()
+  ]
+})
+export class AuthModule {}
